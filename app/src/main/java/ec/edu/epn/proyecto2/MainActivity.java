64c3ea95fdf768f5bdfe9bea9247f5c91a520f3a
CARLOSP7994 <carlangasdavichopt@hotmail.com>
2016-12-11 19:17:17 -0500

    base de datos de lista de buses
    editar, eliminar, modificar

--- app/src/main/java/ec/edu/epn/proyecto2/MainActivity.java
+++ app/src/main/java/ec/edu/epn/proyecto2/MainActivity.java
@@ -1,12 +1,29 @@
 package ec.edu.epn.proyecto2;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 
 public class MainActivity extends AppCompatActivity {
+
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
@@ -14,28 +31,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frminicio);
 
+
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void ir(View view){
-        Intent i = new Intent(this,mapa_fiscalizador.class);
+    public void ir(View view) {
+        Intent i = new Intent(this, mapa_fiscalizador.class);
         startActivity(i);
 
     }
 
-    public void flujounidades(View view){
-        Intent flujo = new Intent(this,flujo.class);
+    public void flujounidades(View view) {
+        Intent flujo = new Intent(this, flujo.class);
         startActivity(flujo);
     }
 
-    public void irUnidad(View View){
-        Intent irUnidad = new Intent(this,crearbus.class);
+    public void irUnidad(View View) {
+        Intent irUnidad = new Intent(this, crearbus.class);
         startActivity(irUnidad);
     }
 
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
 
+    @Override
+    public void onStop() {
+        super.onStop();
 
-
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }

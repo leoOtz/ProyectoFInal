95456a9c95aa6309f52f55d6c9ca97de0d63c3ee
Juan <juan.benavides@epn.edu.ec>
2017-02-10 20:15:50 -0500

    Servicios Rest crear Bus

---
+++ app/src/main/java/ec/edu/epn/proyecto2/Utilitarios/DireccionIP.java
@@ -0,0 +1,10 @@
+package ec.edu.epn.proyecto2.Utilitarios;
+
+/**
+ * Created by Juan on 10/02/2017.
+ */
+
+public class DireccionIP
+{
+  public static   final String ip="localhost:8080/ProyectoMoviles/rest/";
+}

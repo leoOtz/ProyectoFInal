e886260dbc8d3d9076d3c35a6e5dbfdfdb1c8f30
CARLOSP7994 <carlangasdavichopt@hotmail.com>
2017-02-11 11:00:51 -0500

    preferencias

--- app/src/main/java/ec/edu/epn/proyecto2/ajustes.java
+++ app/src/main/java/ec/edu/epn/proyecto2/ajustes.java
@@ -1,21 +1,62 @@
 package ec.edu.epn.proyecto2;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class ajustes extends AppCompatActivity {
 
 
     android.widget.TextView TV;
+    private Spinner cmbNotificacion;
+    private Spinner cmbSonido;
+    private Spinner cmbLed;
+    private Spinner cmbFlash;
+
+
+    private SharedPreferences preferencias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajustes);
+
+        cmbNotificacion = (Spinner)findViewById(R.id.cmbNotificacion);
+        String []datos = new String[]{"Vibracion","Sonido","Vibracion y Sonido"};
+        ArrayAdapter<String> aa = new ArrayAdapter<String>
+                (this,android.R.layout.simple_list_item_1,
+                        datos);
+        cmbNotificacion.setAdapter(aa);
+
+
+        cmbSonido = (Spinner)findViewById(R.id.cmbSonido);
+        String []dato = new String[]{"Big_Easy","BirdLoop","Cairo","Bollywod","Club_Cubano"};
+        ArrayAdapter<String> bb = new ArrayAdapter<String>
+                (this,android.R.layout.simple_list_item_1,
+                        dato);
+        cmbSonido.setAdapter(bb);
+
+
+        cmbLed = (Spinner)findViewById(R.id.cmbLed);
+        String []datosled = new String[]{"Azul","Roja","Amarilla","Cyan"};
+        ArrayAdapter<String> cc = new ArrayAdapter<String>
+                (this,android.R.layout.simple_list_item_1,
+                        datosled);
+        cmbLed.setAdapter(cc);
+
+
+        cmbFlash = (Spinner)findViewById(R.id.cmbFlash);
+        String []datosflash = new String[]{"Encendido","Apagado"};
+        ArrayAdapter<String>dd = new ArrayAdapter<String>
+                (this,android.R.layout.simple_list_item_1,
+                        datosflash);
+        cmbFlash.setAdapter(dd);
 
     }
 
